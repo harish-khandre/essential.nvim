91ec752e5c3db33faed19b11b0978cdd21be34ba
harish-khandre <harishkhandre2901@gmail.com>
2024-02-12 18:16:46 +0530

    opts

--- lua/opts.lua
+++ lua/opts.lua
@@ -1,7 +1,7 @@
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 
 vim.opt.incsearch = true
 vim.opt.hlsearch = false
